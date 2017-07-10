97180036f4e738fea20156ac89892c9aa014c159
Alex B <me@alexb.io>
2017-07-10 03:43:17 -0700

    use channel cache to stay within rate limits and ignore dms

--- apps/tracking/lib/tracking.ex
+++ apps/tracking/lib/tracking.ex
@@ -37,36 +37,37 @@ defmodule Tracking do
     # Write a "legend" as the first line of the file
     IO.write(guild_file, "guild id: #{guild.id} name: #{guild.name}\n")
 
-    Logger.log :info, "new guild added"
+    Logger.info "new guild added (#{guild.name})"
 
     {:noreply, files}
   end
 
   def handle_cast({:new_message, message}, files) do
     # Since we can't tell if the channel is private from the message, we need to make a call to the discord api
-    {:ok, channel} = Api.get_channel(message.channel_id)
+    # {:ok, channel} = Api.get_channel(message.channel_id)
+    channel = Nostrum.Cache.ChannelCache.get([id: message.channel_id])
+    {status, guild} = Nostrum.Cache.Guild.GuildServer.get(channel_id: message.channel_id)
 
     # check if the channel is a DM channel
-    if channel["type"] == 1 do
-      {:noreply, files}
-    else
-      {_, guild} = Nostrum.Cache.Guild.GuildServer.get(channel_id: message.channel_id)
-
+    if Map.get(channel, "type") != 1 and status == :ok do
       file = files[guild.id]
 
       log_entry = log_string message, channel
 
-      Logger.log :info, "new message ingested"
-      Logger.log :debug, log_entry
+      Logger.info "new message ingested (#{guild.name})"
+      Logger.debug log_entry
 
       IO.write(file, log_entry <> "\n")
+      {:noreply, files}
+    else
+      Logger.info "DM recieved, ignoring"
       {:noreply, files}
     end
   end
 
   defp log_string(message, channel) do
-    result = "guild:#{channel["guild_id"]},"
-    result = result <> "channel:#{channel["id"]},"
+    result = "guild:#{Map.get(channel, "guild_id")},"
+    result = result <> "channel:#{Map.get(channel, "id")},"
     result = result <> "from:#{message.author.id},"
     result = result <> "time:#{message.timestamp}"
     result
